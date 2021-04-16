4dca9d90354af8a56db407be96682c59825339b4
moneybirdmi <money.bird.mi@gmail.com>
2021-04-16 13:57:42 +0500

    Simple list view with a notes class

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -2,25 +2,45 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Note> notes;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        notes = (ArrayList<Note>) intent.getSerializableExtra("list");
 
-        String[] array = {"Volvo", "BMW", "Ford", "Mazda"};
+        if(notes == null){
+            notes = new ArrayList<Note>();
+            notes.add(new Note("Pakistan"));
+            notes.add(new Note("India"));
+        }
 
+        createView();
+    }
+
+    private void createView(){
         ListView view = new ListView(this);
         view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        String [] array = new String[notes.size()];
+        for(int i=0; i < notes.size(); i++){
+            array[i] = notes.get(i).getContent();
+        }
         ArrayAdapter adapter = new ArrayAdapter(this,R.layout.simple_list,array);
         view.setAdapter(adapter);
         setContentView(view);
-
     }
 }
---
+++ app/src/main/java/com/example/app1/Note.java
@@ -0,0 +1,35 @@
+package com.example.app1;
+
+import java.util.Date;
+import java.util.UUID;
+import java.io.Serializable;
+
+public class Note implements Serializable{
+
+    private String id;
+    private String content;
+    private Date creationDateTime;
+
+    public Note(){
+        init();
+    }
+
+    public Note(String content){
+        init();
+        this.content = content;
+    }
+
+    private void init(){
+        this.id = UUID.randomUUID().toString();
+        this.creationDateTime = new Date();
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+}
